ae3c425717788327a3799c84ae7013472c76e477
mohamedebrahemerT <mohamedchi2013@gmail.com>
2022-05-03 06:39:09 +0200

    upload
    upload

--- database/migrations/2022_05_02_011915_create_product_variants_table.php
+++ database/migrations/2022_05_02_011915_create_product_variants_table.php
@@ -16,15 +16,17 @@ class CreateProductVariantsTable extends Migration
         Schema::create('product_variants', function (Blueprint $table) {
             $table->id();
 
-              $table->bigInteger('product_id')->unsigned()->nullable();
-            $table->foreign('product_id')->references('id')->on('attributes')->onDelete('cascade');
- 
+                     $table->integer('product_id')->unsigned()->nullable();
+             $table->foreign('product_id')->references('id')->on('productes')->onDelete('cascade');    
 
                $table->bigInteger('attribute_id')->unsigned()->nullable();
             $table->foreign('attribute_id')->references('id')->on('attributes')->onDelete('cascade');
 
                $table->bigInteger('attribute_value_id')->unsigned()->nullable();
             $table->foreign('attribute_value_id')->references('id')->on('attribute_values')->onDelete('cascade');
+
+
+          
 
 
               $table->string('value_ar');
